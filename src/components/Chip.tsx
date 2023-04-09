ab6c4de40234bb2ca81b2c8812928d72bced8250
Arnob Chowdhury <chowdhury.sami.al@gmail.com>
2023-04-09 12:33:58 +0600

    Chip center aligned and max width

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -4,7 +4,7 @@ interface ChipProps {
 
 export const Chip = ({ text }: ChipProps) => {
   return (
-    <div className="rounded-full px-4 py-2 text-xs inline-block bg-color-gray-light">
+    <div className="rounded-full px-4 py-2 text-xs inline-block bg-color-gray-light max-w-[350px] text-center">
       {text}
     </div>
   );
